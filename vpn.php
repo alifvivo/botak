83b979f89fad32bd6bce7a474fe702d17ee469f1
khairul <88110188+alifvivo@users.noreply.github.com>
2021-08-15 00:06:10 +0800

    Add files via upload

---
+++ vpn.php
@@ -0,0 +1,39 @@
+{"Version":"1.01",
+
+"Url":"https://github.com/alifvivo/botak/raw/main/botak.zip" ,
+
+"Changelog":"  
+
+🙏 WELCOME TO PAKYAVPN OVPN🙏
+
+            PAKYA  VPN Stable
+
+PAKYAVPN OVPN is a free and VIP  app for those who want to access Internet securely and get more faster download speed. 
+
+What we privode in our app
+
+🟡 DIGI 🇸🇬 🔜
+
+
+Our server location
+🇸🇬 🔜
+
+Please choose the nearest server to your location for connection stability and get high-speed performance 
+
+Hope you all enjoy and thank you for using our app
+
+",
+
+"latestVersion": "1.01",
+
+"latestVersionCode": "1",
+
+"url": "",
+
+ "releaseNotes": [
+
+ "-Demo"
+
+  ]
+
+}
