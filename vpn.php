9eabb360c6be88b8b101a139141138dadc5052e2
khairul <88110188+alifvivo@users.noreply.github.com>
2021-09-10 23:13:19 +0800

    Add files via upload

---
+++ vpn.php
@@ -0,0 +1,59 @@
+{
+//Versi untuk mengemas kini, jika ada kemas kini, perlu menukar lebih banyak nombor daripada sebelumnya.
+"Version":"1.7
+
+🙏 WELCOME TO PAKYAVPN OVPN🙏
+
+            PAKYA  VPN Stable
+
+PAKYAVPN OVPN is a free and VIP  app for those who want to access Internet securely and get more faster download speed. 
+
+What we privode in our app
+
+🟡 DIGI 🇸🇬 🔜
+🇸🇬NO MULTI LOGIN 🇸🇬
+🇸🇬NO TORREN 🇸🇬
+🇸🇬NO DDOS 🇸🇬
+
+🔴 MAXIS 🇸🇬 🔜
+🇸🇬NO MULTI LOGIN 🇸🇬
+🇸🇬NO TORREN 🇸🇬
+🇸🇬NO DDOS 🇸🇬
+
+🟠 UMOBILE 🇸🇬 🔜
+🇸🇬NO MULTI LOGIN 🇸🇬
+🇸🇬NO TORREN 🇸🇬
+🇸🇬NO DDOS 🇸🇬
+
+
+Our server location
+🇸🇬 🔜
+
+Please choose the nearest server to your location for connection stability and get high-speed performance 
+
+Hope you all enjoy and thank you for using our app
+
+#. Can't update Press to clear app data.!!! 
+#. In the application there are ads.",
+//Pautan untuk kemas kini pelayan
+"Url":"https://github.com/alifvivo/botak/raw/main/botak.zip" ,
+"Changelog":"
+
+
+ 
+
+",
+ 
+ 
+ 
+ 
+ 
+ 
+//Jangan main-main dengan bahagian bawah
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
